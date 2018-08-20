0c0445ae4f8ff77181a82113d015f60779fb27b9
mrinali0123 <6871push@gmail.com>
2018-08-20 14:22:23 +0530

    merging master and naviBranch

---
+++ src/json_data/map1.js
@@ -0,0 +1,44 @@
+
+// "options":[
+//     {"key":"new 1" ,"text" :"New 1", "value":"new 1"},
+//     {"key":"new 2" ,"text" :"New 2", "value":"new 2"},
+//     {"key":"new 3" ,"text" :"New 3", "value":"new 3"}
+
+// ],
+// "country":[{"key":"new 1" ,"text" :"Country 1", "value":"new 1"},
+//     {"key":"new 2" ,"text" :"Country  2", "value":"new 2"},
+//     {"key":"new 3" ,"text" :"Country  3", "value":"new 3"}]
+
+//  import data from './json-data.json';
+
+//  export const country=[]
+ export const status =[
+    {key:'status1' ,text :'status 1-5', value:'status1'},
+    {key:'status2' ,text :'status 6-10', value:'status2'},
+    {key:'status3' ,text :'status 11-15', value:'status3'}
+]
+export const AddTarget =[
+    {key: "add target 1" ,text :"Add Target 1", value : "add target 1"},
+    {key: "add target 2" ,text :"Add Target 2", value : "add target 2"},
+    {key: "add target 3" ,text :"Add Target 3", value : "add target 3"}
+]
+export const ByPractice =[
+    {key: "By Practice 1" ,text :"By Practice 1", value : "By Practice 1"},
+    {key: "By Practice 2" ,text :"By Practice 2", value : "By Practice 2"},
+    {key: "By Practice 3" ,text :"By Practice 3", value : "By Practice 3"}
+]
+export const Measures =[
+    {key: "Measures 1" ,text :"Measures 1", value : "Measures 1"},
+    {key: "Measures 2" ,text :"Measures 2", value : "Measures 2"},
+    {key: "Measures 3" ,text :"Measures 3", value : "Measures 3"}
+]
+
+export const cntr =[
+    {key: "UCLA cntr 1" ,text :"UCLA cntr 1", value : "UCLA cntr 1"},
+    {key: "UCLA cntr 2" ,text :"UCLA cntr 2", value : "UCLA cntr 2"},
+    {key: "UCLA cntr 3" ,text :"UCLA cntr 3", value : "UCLA cntr 3"}
+]
+
+    
+
+   
